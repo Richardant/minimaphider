3a6f8bc714a7626344fc052d777a86cc81667d8d
Richardant <54707022+Richardant@users.noreply.github.com>
2025-06-20 11:46:11 -0400

    Add minimaphider

---
+++ src/main/java/com/minimaphider/MinimapHiderPlugin.java
@@ -0,0 +1,62 @@
+package com.minimaphider;
+
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+
+import net.runelite.api.Client;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.gameval.InterfaceID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@Slf4j
+@PluginDescriptor(
+        name = "Minimap Hider",
+        description = "Hides minimap",
+        tags = {"minimap", "hide"}
+)
+public class MinimapHiderPlugin extends Plugin
+{
+    @Inject
+    private Client client;
+
+    @Override
+    protected void startUp()
+    {
+        setMapHidden(true);
+    }
+
+    @Override
+    protected void shutDown()
+    {
+        setMapHidden(false);
+    }
+
+    @Subscribe
+    public void onWidgetLoaded(WidgetLoaded event)
+    {
+        if (event.getGroupId() == InterfaceID.TOPLEVEL_OSRS_STRETCH || event.getGroupId() == InterfaceID.TOPLEVEL_PRE_EOC)
+        {
+            setMapHidden(true);
+        }
+    }
+
+    private void setMapHidden(boolean hidden)
+    {
+        Widget orb = client.getWidget(InterfaceID.TOPLEVEL_OSRS_STRETCH, 22);
+        if (orb != null)
+        {
+            orb.setHidden(hidden);
+        }
+        for (int i = 23; i <= 32; i++)
+        {
+            Widget w = client.getWidget(InterfaceID.TOPLEVEL_PRE_EOC, i);
+            if (w != null)
+            {
+                w.setHidden(hidden);
+            }
+        }
+    }
+}
